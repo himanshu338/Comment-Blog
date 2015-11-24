ad91f7083138f009484ce2dec09a86cdcc28212a
himanshu338 <bansalhimanshu338@gmail.com>
2015-11-24 17:05:22 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+require_once("database.php");
+require_once("functions.php");
+require_once("session.php");
+require_once("user.php");
+
+if(isset($_GET["username"]) && isset($_GET["password"]))
+{
+	$username=$_GET["username"];
+	$password=$_GET["password"];
+	$found_user=User::authenticate($username,$password);
+	if($found_user){
+		$session->login($found_user);
+		redirect_to("main.php");
+	}
+	else{
+		die("record not found".mysql_error());
+		//echo "record not found";
+		//redirect_to("login.html");
+		
+	}
+}
+?>

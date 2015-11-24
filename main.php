9125f075d97df54a3b83f7722bfe7476e76c3aa7
himanshu338 <bansalhimanshu338@gmail.com>
2015-11-24 17:07:50 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once("database.php");
+require_once("functions.php");
+require_once("session.php");
+require_once("user.php");
+
+if(!$session->is_logged_in()){redirect_to("login.html");}
+?>
+<html>
+<head>
+<title>
+comment 
+</title>
+</head>
+<form id="3" method="GET" action="comment.php">
+Comment:</br></br>
+<input type="text" rows="16" cols="24" value="area" name="area"></br>
+<input type="submit"></br></br>
+</form>
+</html>
+
+<?php
+
+require_once("ex2.php");
+
+?>
+
+<html>
+<form id="4" method="GET" action="logout.php">
+<input type="submit" name="logout"  value="logout">
+</form>
+</html>
+</br>
+</br>
+</form>
+</html>
+
+
+	
+
+

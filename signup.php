7baab2566557f51aa440196d26f1bc811fe39e91
himanshu338 <bansalhimanshu338@gmail.com>
2015-11-24 17:06:25 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+require_once("database.php");
+require_once("functions.php");
+require_once("session.php");
+require_once("user.php");
+ 
+if(isset($_GET["username"]) && isset($_GET["password"]) && isset($_GET["firstname"]) && isset($_GET["lastname"]))
+{	
+
+	$user=new User();
+	$user->username=$_GET["username"];
+	$user->password=$_GET["password"];
+	$user->firstname=$_GET["firstname"];
+	$user->lastname=$_GET["lastname"];
+	$user->create();
+}
+else{
+ 	echo "all detials are not filled";
+}
+
+
+?>
